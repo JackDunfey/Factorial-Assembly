3d250888a9f0b38d2d6d614a3d0584d361329f60
Jack <62919016+JackDunfey@users.noreply.github.com>
2023-03-02 21:28:20 -0500

    Update factorial.s
    Runs in bare machine

--- factorial.s
+++ factorial.s
@@ -9,26 +9,31 @@
 
         slti $t0, $a0, 3 #t0 = a0 < 3
         bne $t0, $zero, RETURN_N #return n if t0 != 0
-        add $t0, $zero, $t0 #nop
+        add $0, $0, $0 #nop
         
-        jal factorial
         addi $a0, $a0, -1 #store factorial n-1 in v0
+        jal factorial
+        add $0, $0, $0
 
         lw $a0, 0($sp)
+        add $0, $0, $0 #nop
 
         # return n * factorial (n-1)
         mult $a0, $v0
-        j RETURN
         mflo $v0
+        j RETURN
+        add $0, $0, $0 #nop
 
         RETURN_N: add $v0, $zero, $a0
 
         RETURN:
 
         lw $ra, 4($sp)
+        add $0, $0, $0 #nop
 
-        jr $ra
         addi $sp, $sp, 8
+        jr $ra
+        add $0, $0, $0 #nop
 
     main:
         # 0x10000000 = 0x1000 0000 = 4096 0
@@ -51,8 +56,9 @@
         syscall
 
 
-        jal factorial
         add $a0, $zero, $s1
+        jal factorial
+        add $0, $0, $0 #nop
 
         add $a0, $zero, $v0
         addi, $v0, $zero, 1
